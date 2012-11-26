87cc45fb3fb5b68e104b93987b2d8dc328001bbf
Samu Voutilainen <smar@smar.fi>
2012-11-26 11:14:38 +0200

    a script to run antlrphpruntime

---
+++ gen-antlr-php.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/env ruby
+
+require "trollop"
+
+# #
+# Uses antlr to generate PHP targets
+#
+# This script is written in Ruby as I don’t really like PHP and I needed the launcher.
+# Feel free to contribute a launcher done in some other language.
+#
+
+p = Trollop::Parser.new do
+    opt :grammar, "Grammar file to process", type: :string
+    opt :libdir, "Directory where antlrphpruntime is", type: :string
+end
+
+opts = Trollop::with_standard_exception_handling p do
+    args = ARGV.clone
+    result = p.parse ARGV
+
+    # show help screen
+    raise Trollop::HelpNeeded if args.empty?
+    raise Trollop::HelpNeeded if result[:grammar].nil?
+
+    result
+end
+
+if opts[:libdir].nil?
+    LIBDIR = "/usr/local/share/antlr-php"
+else
+    LIBDIR = opts[:libdir]
+end
+
+grammar = opts[:grammar]
+
+Dir.chdir File.dirname(__FILE__) do
+    puts `java -cp #{LIBDIR} org.antlr.Tool #{grammar_file}`
+end
--- install.rb
+++ install.rb
@@ -9,12 +9,16 @@ opts = Trollop::options do
 end
 
 INSTALLED_FILE = ".installed_files.json"
-DESTINATION_DIR = "/usr/local/share/antlr-php"
+D_SHARE_DIR = "/usr/local/share/antlr-php"
+D_BIN_DIR = "/usr/local/bin"
 
 files_to_be_installed = {
     "lib" => [
         "antlr-3.4-complete.jar",
         "antlr-php.jar"
+    ],
+    "bin" => [
+        "gen-antlr-php.rb"
     ]
 }
 
@@ -29,7 +33,8 @@ unless installed_files.nil?
         FileUtils.rm file
     end
 
-    FileUtils.rmdir DESTINATION_DIR
+    FileUtils.rmdir D_SHARE_DIR
+    FileUtils.rmdir D_BIN_DIR
     FileUtils.rm INSTALLED_FILE
 end
 
@@ -41,12 +46,26 @@ end
 
 installed_files = []
 
-FileUtils.mkdir_p DESTINATION_DIR
+FileUtils.mkdir_p D_SHARE_DIR
 lib_files = files_to_be_installed["lib"]
+bin_files = files_to_be_installed["bin"]
 
 lib_files.each do |file|
-    FileUtils.cp "lib/#{file}", DESTINATION_DIR
-    installed_files << "#{DESTINATION_DIR}/#{file}"
+    FileUtils.cp "lib/#{file}", D_SHARE_DIR
+    installed_files << "#{D_SHARE_DIR}/#{file}"
+end
+
+bin_files.each do |file|
+    FileUtils.cp file, D_BIN_DIR
+
+    fullfile = "#{D_BIN_DIR}/#{file}"
+    basename = "#{D_BIN_DIR}/#{File.basename(file, File.extname(file))}"
+
+    File.chmod 0755, fullfile
+    FileUtils.ln_s fullfile, basename
+
+    installed_files << fullfile
+    installed_files << basename
 end
 
 content = JSON.pretty_generate installed_files
