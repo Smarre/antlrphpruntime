a9128508743cc27ec92c0a55730766d0f5acc855
Samu Voutilainen <smar@smar.fi>
2013-06-12 15:40:41 +0300

    make printids.php to find Antlr headers from correct location

--- test/printids.php
+++ test/printids.php
@@ -6,7 +6,7 @@ spl_autoload_register(
 
     function ($class) {
         if (strpos($class, "Antlr\Runtime") !== false) {
-            require __DIR__ . "/../runtime/Php/src/" .
+            require "../" .
 		str_replace("\\", DIRECTORY_SEPARATOR, $class) . ".php";
         } else if (strpos($class, "Antlr\Tests") !== false) {
             require __DIR__ . "/" . str_replace("\\", DIRECTORY_SEPARATOR, $class) . ".php";
